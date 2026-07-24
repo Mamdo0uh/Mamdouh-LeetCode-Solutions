ab8f94e05d5ad5d1fe3b42db6a8dbb692bf5184f
Mohammed Mamdouh <mamdouhd845@gmail.com>
2026-07-24 16:07:36 +0300

    Time: 1030 ms (86.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,4 @@
+select SUBSTRING(CAST(trans_date AS VARCHAR),1,7) as month,country,count(id) as trans_count,
+sum(case when state ='approved' then 1 else 0 end) as approved_count,sum(amount) as trans_total_amount,sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by SUBSTRING(CAST(trans_date AS VARCHAR),1,7),country
