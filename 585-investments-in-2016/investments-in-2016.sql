c0514c85b8dfa8c4aec3a94dfbd5a7337fe41ddb
Mohammed Mamdouh <mamdouhd845@gmail.com>
2026-08-05 21:18:45 +0300

    Time: 1001 ms (5.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,9 @@
+SELECT ROUND(CAST(SUM(tiv_2016) AS DECIMAL(18,2)), 2) AS tiv_2016
+FROM insurance
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM insurance GROUP BY tiv_2015
+    HAVING COUNT(pid) > 1
+) 
+AND CONCAT(lat, ',', lon) IN ( SELECT CONCAT(lat, ',', lon) FROM insurance
+GROUP BY lat, lon
+    HAVING COUNT(pid) = 1
+)
