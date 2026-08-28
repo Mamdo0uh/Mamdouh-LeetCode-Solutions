75451343d3869de3d697015e1215c76f94552976
Mohammed Mamdouh <mamdouhd845@gmail.com>
2026-08-28 03:43:59 +0300

    Time: 265 ms (87.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -0,0 +1,3 @@
+select  transaction_date,sum(case when amount%2 <> 0 then amount else 0 end) as odd_sum ,sum(case when amount%2 = 0 then amount else 0 end) as even_sum
+from transactions
+group by transaction_date
